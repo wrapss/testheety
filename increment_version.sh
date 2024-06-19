2769e6613d21860df1dc25e869eb885de9915e3c
wraps <me@matthieu.pw>
2024-06-19 15:11:37 +0200

    chore(deploy): Update deployment trigger and versioning script
    - Change the trigger for the deployment workflow from main branch to tags.
    - Improve the version increment script to use latest tag as version name.

--- increment_version.sh
+++ increment_version.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Lire la version actuelle du fichier pubspec.yaml
-version=$(grep 'version:' pubspec.yaml | sed 's/version: //')
-version_name=$(echo $version | cut -d "+" -f 1)
-build_number=$(echo $version | cut -d "+" -f 2)
+latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-# Incrémenter le numéro de build
-new_build_number=$((build_number + 1))
+version_name=$(echo $latest_tag | cut -d "-" -f 1)
 
-# Mettre à jour le fichier pubspec.yaml avec la nouvelle version
-sed -i.bak "s/version: $version_name+$build_number/version: $version_name+$new_build_number/" pubspec.yaml
+current_version=$(grep 'version:' pubspec.yaml | sed 's/version: //')
+current_build_number=$(echo $current_version | cut -d "+" -f 2)
+
+if [ -z "$current_build_number" ]; then
+  current_build_number=0
+fi
+
+new_build_number=$((current_build_number + 1))
+
+sed -i.bak "s/version: .*/version: $version_name+$new_build_number/" pubspec.yaml
 
 echo "Version incrémentée à $version_name+$new_build_number"
